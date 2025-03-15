94df9c6823929cc9bd217d08495331c1f4500237
manhd89 <150607244+manhd89@users.noreply.github.com>
2025-03-15 14:55:32 +0700

    test

--- apkfree.sh
+++ apkfree.sh
@@ -4,8 +4,39 @@ req() {
     wget --header="User-Agent: Mozilla/5.0" -nv -O "$@"
 }
 
-url="https://androidapksfree.com/youtube/com-google-android-youtube/old/"
+# Find max version
+max() {
+	local max=0
+	while read -r v || [ -n "$v" ]; do
+		if [[ ${v//[!0-9]/} -gt ${max//[!0-9]/} ]]; then max=$v; fi
+	done
+	if [[ $max = 0 ]]; then echo ""; else echo "$max"; fi
+}
 
-url=$(req - $url | grep 'class="limit-line">19.47.53' -B1 | grep -oP 'href="\K[^"]+')
+# Read highest supported versions from Revanced 
+get_supported_version() {
+    package_name=$1
+    output=$(java -jar revanced-cli*.jar list-versions -f "$package_name" patch*.rvp)
+    version=$(echo "$output" | tail -n +3 | sed 's/ (.*)//' | grep -v -w "Any" | max | xargs)
+    echo "$version"
+}
+
+# Download necessary resources to patch from Github latest release 
+download_resources() {
+    for repo in revanced-patches revanced-cli; do
+        githubApiUrl="https://api.github.com/repos/revanced/$repo/releases/latest"
+        page=$(req - 2>/dev/null $githubApiUrl)
+        assetUrls=$(echo $page | jq -r '.assets[] | select(.name | endswith(".asc") | not) | "\(.browser_download_url) \(.name)"')
+        while read -r downloadUrl assetName; do
+            req "$assetName" "$downloadUrl" 
+        done <<< "$assetUrls"
+    done
+}
+
+download_resources
+
+url="https://androidapksfree.com/youtube/com-google-android-youtube/old/"
+version="${version:-$(get_supported_version "com.google.android.youtube")}"
+url=$(req - $url | grep 'class="limit-line">$version' -B1 | grep -oP 'href="\K[^"]+')
 url=$(req - $url | grep 'class="buttonDownload box-shadow-mod"' | grep -oP 'href="\K[^"]+')
 req youtube.apk $url
