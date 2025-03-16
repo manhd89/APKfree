05ab1c915413b497921c7be1e8c11d447fd21c7c
manhd89 <150607244+manhd89@users.noreply.github.com>
2025-03-16 15:08:32 +0700

    upgrade req

--- apkfree.sh
+++ apkfree.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 req() {
-    wget --header="User-Agent: Mozilla/5.0" -nv -O "$@"
+    wget --header="User-Agent: Mozilla/5.0 (Android 13; Mobile; rv:125.0) Gecko/125.0 Firefox/125.0" \
+         --header="Content-Type: application/octet-stream" \
+         --header="Accept-Language: en-US,en;q=0.9" \
+         --header="Connection: keep-alive" \
+         --header="Upgrade-Insecure-Requests: 1" \
+         --header="Cache-Control: max-age=0" \
+         --header="Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8" \
+         --keep-session-cookies --timeout=30 -nv -O "$@"
 }
 
 # Find max version
